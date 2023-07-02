db6b6bbe65fbfcd7fb8fe7ba6d092179686ae3ba
srxvathsav <sreevathsav2002@gmail.com>
2023-07-02 19:01:38 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+import face_recognition
+
+imgElon = face_recognition.load_image_file('Timg\Elon.jpg')
+imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
+
+imgTest = face_recognition.load_image_file('Timg\ElonT.jpg')
+imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
+
+faceLoc = face_recognition.face_locations(imgElon)[0]
+encodeElon = face_recognition.face_encodings(imgElon)[0]
+cv2.rectangle(imgElon, (faceLoc[0], faceLoc[1]),
+              (faceLoc[2], faceLoc[3]), (255, 0, 255), 2)
+
+
+faceLocT = face_recognition.face_locations(imgTest)[0]
+encodeElonT = face_recognition.face_encodings(imgTest)[0]
+cv2.rectangle(imgTest, (faceLocT[3], faceLocT[0]),
+              (faceLocT[1], faceLocT[2]), (255, 0, 255), 2)
+
+result = face_recognition.compare_faces([encodeElon], encodeElonT)
+faceDis = face_recognition.face_distance([encodeElon], encodeElonT)
+
+cv2.putText(imgTest, f'{result} {round(faceDis[0],2)}', (
+    50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+
+print(result, faceDis)
+
+cv2.imshow('Elon Musk', imgElon)
+cv2.imshow('Elon Test', imgTest)
+cv2.waitKey(0)
